47d6b597b06adebfdb9f9adb6243b52acc6e5d16
BramTempelaere <bobdebouwer381@gmail.com>
2026-06-20 18:38:07 +0200

    Initial Compass PWA

---
+++ sw.js
@@ -0,0 +1,27 @@
+const CACHE = 'compass-v1';
+const SHELL = ['./index.html', './manifest.json', './sw.js'];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(SHELL)));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', e => {
+  // App shell: cache-first. Everything else: network-first.
+  const url = new URL(e.request.url);
+  const isShell = SHELL.some(s => url.pathname.endsWith(s.replace('./', '/')));
+  if (isShell) {
+    e.respondWith(caches.match(e.request).then(r => r || fetch(e.request)));
+  } else {
+    e.respondWith(fetch(e.request).catch(() => caches.match(e.request)));
+  }
+});
